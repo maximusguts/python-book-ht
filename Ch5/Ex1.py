bb79d83edd509164f55fa1f73bde51eec9cddd69
maximusguts <maximguts@gmail.com>
2022-08-13 17:22:21 +0300

    Ch5: Ex1 done

---
+++ Ch5/Ex1.py
@@ -0,0 +1,9 @@
+from random import *
+
+money = randint(0, 2000)
+if money > 1000:
+    print("я багатый")
+    print("ха-ха")
+else:
+    print("я не багатый")
+    print("но может когда ни буть и буду...")
