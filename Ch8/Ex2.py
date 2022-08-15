18806c5ca3d0ef953fb1a19e8d7d04250063b6ee
maximusguts <maximguts@gmail.com>
2022-08-15 11:34:41 +0300

    Ch8: Ex2 done

---
+++ Ch8/Ex2.py
@@ -0,0 +1,39 @@
+import turtle
+import time
+
+max = turtle.Pen()
+sem = turtle.Pen()
+andrew = turtle.Pen()
+roby = turtle.Pen()
+
+m = max
+s = sem
+a = andrew
+r = roby
+
+m.fd(50)
+s.fd(55)
+a.fd(55)
+r.fd(50)
+
+m.lt(90)
+s.lt(90)
+a.rt(90)
+r.rt(90)
+
+m.fd(30)
+s.fd(20)
+a.fd(20)
+r.fd(30)
+
+m.rt(90)
+s.rt(90)
+a.lt(90)
+r.lt(90)
+
+m.fd(40)
+s.fd(25)
+a.fd(25)
+r.fd(40)
+
+time.sleep(3)
