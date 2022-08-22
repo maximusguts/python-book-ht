d60c99ff388b82e56b59dc4060adb755ac76c185
maximusguts <maximguts@gmail.com>
2022-08-22 11:07:24 +0300

    Ch12: Ex1 update

--- Ch12/Ex1.py
+++ Ch12/Ex1.py
@@ -1,9 +1,15 @@
 import random
 from tkinter import *
 tk = Tk()
-canvas = Canvas(tk, width=600, height=600)
+canvas = Canvas(tk, width=900, height=600)
 canvas.pack()
+
+text = canvas.create_text(750, 30, text="Трикутників:")
+
+x = 0
 while True:
+    x = x + 1
+    text = canvas.create_text(750, 50, text=x)
     canvas.create_polygon(random.randint(0, 600),
                           random.randint(0, 600),
                           random.randint(0, 600),
@@ -12,4 +18,6 @@ while True:
                           random.randint(0, 600),
                           fill="green",
                           outline="black")
+
     tk.update()
+    canvas.delete(text)
